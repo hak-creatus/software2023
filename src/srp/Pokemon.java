d4b21831b3e2412bc1dd38016dc88e43484ba3c7
Parker <hak891@gmail.com>
2023-03-11 10:30:17 +0900

    week02 class pokemon

---
+++ src/srp/Pokemon.java
@@ -0,0 +1,37 @@
+package srp;
+
+public class Pokemon {
+    private String name;
+    private int level;
+    private int hp;
+
+    public Pokemon(String name, int level, int hp) {
+        this.name = name;
+        this.level = level;
+        this.hp = hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+}
